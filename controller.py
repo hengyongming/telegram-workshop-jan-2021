52113bc601a868ab576e8d4f38daa39541f84434
DavidCai <60083307+davidcaiqifan@users.noreply.github.com>
2021-01-08 11:28:31 +0800

    Basic-Setup

--- controller.py
+++ controller.py
@@ -1,7 +1,12 @@
+from pprint import pprint
+
+import telebot
 from flask import request
+from mal import Anime
+from pip._internal import commands
 
 from api.dialogflow_api import detect_intent_via_text, detect_intent_via_event
-from api.telegram_api import send_message
+from api.telegram_api import send_message, bot
 from beans.session import Session
 from beans.user import User
 from cache import get_current_session
@@ -22,7 +27,37 @@ def hello_world():
 
 
 # Validates incoming webhook request to make sure required fields are present, before processing
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     # FILL IN CODE
+#     req_body = request.get_json()
+#     user = get_user_from_request(req_body)
+#     session = get_current_session(user)
+#     user_input = get_user_input_from_request(req_body)
+#
+#     if is_not_blank(user.id, user_input):
+#         __process_dialogflow_input(user, session, user_input)
+#     return 'Got your message'
+
+# Validates incoming webhook request to make sure required fields are present, before processing
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # FILL IN CODE
-    return
+    req_body = request.get_json()
+    user = get_user_from_request(req_body)
+    session = get_current_session(user)
+    user_input = get_user_input_from_request(req_body)
+    anime = Anime(1)  # Cowboy Bebop
+    anime.reload()  # reload object
+    bot.send_message(user.id, 'Anime is rated ' + str(anime.score))
+    #send_message(user, session, 'Anime is rated ' + str(anime.score))
+    return 'Anime is rated ' + str(anime.score)
+
+
+# Calls Dialogflow API to trigger an intent match
+# Calls the corresponding function handler for the intent result action if present
+def __process_dialogflow_input(user: User, session: Session, user_input):
+    intent_result = detect_intent_via_text(session.id, user_input)
+    intent_action = default_if_blank(intent_result.action, '')
+    if is_not_blank(intent_action):
+        INTENT_HANDLERS.get(intent_action, handle_invalid_intent)(user, intent_result, session.id)
+    return intent_result
